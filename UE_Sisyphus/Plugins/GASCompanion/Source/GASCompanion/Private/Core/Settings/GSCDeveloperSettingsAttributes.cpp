86ce86456d504c84406246940a5974236fc648a7
haodong.liao <haodong.liao@gmail.com>
2025-11-10 23:01:29 +0800

    Initial project setup for UE_Sisyphus
    Add Unreal Engine project files, configuration, assets, input mappings, character blueprints, level prototypes, and variant modules for shooter and horror gameplay. This commit establishes the base structure and content for the UE_Sisyphus project.

---
+++ UE_Sisyphus/Plugins/GASCompanion/Source/GASCompanion/Private/Core/Settings/GSCDeveloperSettingsAttributes.cpp
@@ -0,0 +1,69 @@
+﻿// Copyright 2021-2024 Mickael Daniel. All Rights Reserved.
+
+#include "Core/Settings/GSCDeveloperSettingsAttributes.h"
+
+#include "AttributeSet.h"
+#include "GSCLog.h"
+#include "Abilities/Attributes/GSCAttributeSet.h"
+#include "UObject/Class.h"
+
+UGSCDeveloperSettingsAttributes::UGSCDeveloperSettingsAttributes()
+{
+}
+
+const UGSCDeveloperSettingsAttributes& UGSCDeveloperSettingsAttributes::Get()
+{
+	const UGSCDeveloperSettingsAttributes* Settings = GetDefault<UGSCDeveloperSettingsAttributes>();
+	check(Settings);
+	return *Settings;
+}
+
+UGSCDeveloperSettingsAttributes& UGSCDeveloperSettingsAttributes::GetMutable()
+{
+	UGSCDeveloperSettingsAttributes* Settings = GetMutableDefault<UGSCDeveloperSettingsAttributes>();
+	check(Settings);
+	return *Settings;
+}
+
+FName UGSCDeveloperSettingsAttributes::GetCategoryName() const
+{
+	return PluginCategoryName;
+}
+
+#if WITH_EDITOR
+FText UGSCDeveloperSettingsAttributes::GetSectionText() const
+{
+	return NSLOCTEXT("GSCDeveloperSettingsAttributes", "Attributes", "Attributes");
+}
+
+void UGSCDeveloperSettingsAttributes::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	
+	static const FName HideInDetailsViewPropertyName = GET_MEMBER_NAME_CHECKED(UGSCDeveloperSettingsAttributes, bShouldHideGSCAttributeSetInDetailsView);
+	const FName PropertyName = PropertyChangedEvent.Property ? PropertyChangedEvent.Property->GetFName() : NAME_None;
+
+	if (PropertyName == HideInDetailsViewPropertyName)
+	{
+		SetHideInDetailsViewMetaData(UGSCAttributeSet::StaticClass(), bShouldHideGSCAttributeSetInDetailsView);
+	}
+}
+
+void UGSCDeveloperSettingsAttributes::SetHideInDetailsViewMetaData(UClass* InClass, const bool bInHideInDetailsView)
+{
+	check(InClass);
+	checkf(InClass->IsChildOf(UAttributeSet::StaticClass()), TEXT("InClass %s is not a UAttributeSet class"), *InClass->GetName());
+
+	static const FString Key = TEXT("HideInDetailsView");
+	if (bInHideInDetailsView)
+	{
+		InClass->SetMetaData(*Key, TEXT("true"));
+		GSC_PLOG(Verbose, TEXT("%s metadata has been set for %s class"), *Key, *InClass->GetName())
+	}
+	else
+	{
+		InClass->RemoveMetaData(*Key);
+		GSC_PLOG(Verbose, TEXT("%s metadata has been removed for %s class"), *Key, *InClass->GetName())
+	}
+}
+#endif
---
+++ UE_Sisyphus/Plugins/GASCompanion/Source/GASCompanion/Public/Core/Settings/GSCDeveloperSettingsAttributes.h
@@ -0,0 +1,55 @@
+﻿// Copyright 2021-2024 Mickael Daniel. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/DeveloperSettings.h"
+#include "GSCDeveloperSettingsAttributes.generated.h"
+
+class UAttributeSet;
+
+/**
+ * Developer Settings for GAS Companion, Attributes and AttributeSets related config.
+ *
+ * Upon changing any of the below configuration settings, the `Config/DefaultGame.ini` file will be saved to persist
+ * this setting.
+ */
+UCLASS(config=Game, defaultconfig)
+class GASCOMPANION_API UGSCDeveloperSettingsAttributes : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Enable this setting to hide GSCAttributeSet attributes from appearing in the Gameplay Attributes dropdown,
+	 * specifically in Gameplay Effects and K2 nodes like GetFloatAttribute() and their Attribute pin parameter.
+	 */
+	UPROPERTY(config, EditAnywhere, Category = "Attributes")
+	bool bShouldHideGSCAttributeSetInDetailsView = false;
+
+	/** Default constructor */
+	UGSCDeveloperSettingsAttributes();
+
+	static const UGSCDeveloperSettingsAttributes& Get();
+	static UGSCDeveloperSettingsAttributes& GetMutable();
+
+	/**
+	 * The category name for our developer settings
+	 *
+	 * @see GetCategoryName
+	 */
+	static constexpr const TCHAR* PluginCategoryName = TEXT("GAS Companion");
+
+	//~ Begin UDeveloperSettings interface
+	virtual FName GetCategoryName() const override;
+#if WITH_EDITOR
+	virtual FText GetSectionText() const override;
+	//~ End UDeveloperSettings interface
+	
+	//~ Begin UObject interface
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	//~ End UObject interface
+
+	/** Adds or remove `HideInDetailsView` class metadata to the passed in UClass. InClass must be a child of UAttributeSet */
+	static void SetHideInDetailsViewMetaData(UClass* InClass, bool bInHideInDetailsView);
+#endif
+};
